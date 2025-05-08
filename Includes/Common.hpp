c85a4c2678cd1a88c660147489aecbb8b7113e08
joaoviictorti <joaovictorti08@gmail.com>
2025-05-08 00:24:11 -0300

    Update Common.hpp

--- Includes/Common.hpp
+++ Includes/Common.hpp
@@ -9,6 +9,30 @@
 SEC_ATTR(".data") CHAR* Output = 0;
 SEC_ATTR(".data") WORD  CurrentBufferSize = 0;
 
+// Macro to cast a value to a generic pointer
+template<typename T>
+constexpr auto PTR(T v) -> PVOID {
+    return reinterpret_cast<PVOID>(v);
+}
+
+// Macro to cast a value to an unsigned pointer-sized integer
+template<typename T>
+constexpr auto U_PTR(T v) -> ULONG_PTR {
+    return reinterpret_cast<ULONG_PTR>(v);
+}
+
+// Macro to cast a value to pointer to bytes
+template<typename T>
+constexpr auto B_PTR(T v) -> PBYTE {
+    return reinterpret_cast<PBYTE>(v);
+}
+
+// Macro to check if a pointer or value is NULL
+template<typename T>
+constexpr auto IS_NULL(T* v) -> BOOL {
+    return v == nullptr;
+}
+
 /**
  * @brief Represents a type that can be dereferenced using the '*' operator.
  *
--- Includes/Macros.hpp
+++ Includes/Macros.hpp
@@ -1,28 +1,5 @@
+// Declares a reference to a dynamically imported function with a decorated name (e.g., ntdll$NtOpenProcess)
 #define DFR(module, function) DECLSPEC_IMPORT decltype(function) module##$##function;
 
 // Macro to place a variable or function in a specific binary section
 #define SEC_ATTR(X) [[gnu::section(X)]]
-
-// Macro to cast a value to a generic pointer
-template<typename T>
-constexpr auto PTR(T v) -> PVOID {
-    return reinterpret_cast<PVOID>(v);
-}
-
-// Macro to cast a value to an unsigned pointer-sized integer
-template<typename T>
-constexpr auto U_PTR(T v) -> ULONG_PTR {
-    return reinterpret_cast<ULONG_PTR>(v);
-}
-
-// Macro to cast a value to pointer to bytes
-template<typename T>
-constexpr auto B_PTR(T v) -> PBYTE {
-    return reinterpret_cast<PBYTE>(v);
-}
-
-// Macro to check if a pointer or value is NULL
-template<typename T>
-constexpr auto IS_NULL(T* v) -> BOOL {
-    return v == nullptr;
-}
